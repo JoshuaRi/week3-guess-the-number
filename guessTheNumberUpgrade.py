414f9424b7748814a639c161c0389745f831530c
JoshuaRi <Jritmo@mail.valenciacollege.edu>
2016-01-31 12:42:52 -0500

    Update guessTheNumberUpgrade.py

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -77,6 +77,9 @@ def evaluateAnswer( userGuess, userSecretNumber ):
 def playGame( showAnswer ):
        print ('Welcome to my number generator game!!')
        print('Please type the highest number you would like to guess for!')
+       myTopLimit = int(input())
+       theNumber = generateNumber(myTopLimit))
+       print(' Guess any number between 1 and ' + str(myTopLimit))
     # TO DO: ####################################################
     # Write code in this function that                          #
     # 1. Greets the user                                        #
